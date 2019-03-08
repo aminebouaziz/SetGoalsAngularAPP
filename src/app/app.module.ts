9f4eaf86222f3c78c943b2a1982978dfa3077079
aminebouaziz <bouaziz.amine.18@gmail.com>
2019-03-08 14:35:56 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AppRoutingModule} from './appRooting.module'
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/appRooting.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from './about/about.component';
+import {Routes,RouterModule} from "@angular/router";
+const routes:Routes=[
+    {
+        path:'',
+        component:HomeComponent
+    },
+    {
+        path:'about',
+        component:AboutComponent
+    }
+];
+
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
